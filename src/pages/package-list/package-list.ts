5b2b2da78c0f8a32a103afdd8fa20753708e121d
iamsakon <sakon.bluechip@gmail.com>
2017-05-29 16:00:07 +0700

    add navParam to package-list prototype
    add navParam in order to specify the module

--- src/pages/package-list/package-list.ts
+++ src/pages/package-list/package-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-package-list',
@@ -7,8 +7,10 @@ import { NavController } from 'ionic-angular';
 })
 export class PackageListPage {
 
-  constructor(public navCtrl: NavController) {
+  selectedModuleItem: any;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	this.selectedModuleItem = navParams.get('moduleItem');
   }
 
 }
